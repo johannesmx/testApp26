b7adfb4c6102d448fbcafd682c5f3ec7803e1038
johannesmx <johannes.muljana@ait.nsw.edu.au>
2026-03-05 22:57:32 +0000

    added and implemented ThemedInput and View

--- app/index.tsx
+++ app/index.tsx
@@ -1,80 +1,84 @@
-import {View,StyleSheet,Text,TextInput,Pressable} from 'react-native'
-import {useState,useEffect} from 'react'
+import { View, StyleSheet, Text, TextInput, Pressable } from 'react-native'
+import { useState, useEffect } from 'react'
 import { Link } from 'expo-router'
 import { ValidationStates } from '@/interfaces/ValidationStates'
 import { useThemeColors } from '@/hooks/useThemeColors'
 import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import { ThemedInput } from '@/components/ThemedInput'
 
 export default function SignupScreen() {
-    const [email,setEmail] = useState <string>("")
-    const [validEmail,setValidEmail] = useState<ValidationStates>(ValidationStates.NONE)
-    const [password,setPassword] = useState<string>("")
-    const [validPassword,setValidPassword] = useState<ValidationStates>(ValidationStates.NONE)
-    
+    const [email, setEmail] = useState<string>("")
+    const [validEmail, setValidEmail] = useState<ValidationStates>(ValidationStates.NONE)
+    const [password, setPassword] = useState<string>("")
+    const [validPassword, setValidPassword] = useState<ValidationStates>(ValidationStates.NONE)
+
     const theme = useThemeColors()
 
-    useEffect( () => {
-        if( email.indexOf('@') > 0 ) {
+    useEffect(() => {
+        if (email.indexOf('@') > 0) {
             setValidEmail(ValidationStates.VALID)
         }
-        else if(email.length > 0 ) {
+        else if (email.length > 0) {
             setValidEmail(ValidationStates.INVALID)
         }
     }, [email])
 
-    useEffect( () => {
-        if( password.length >= 8 ) {
+    useEffect(() => {
+        if (password.length >= 8) {
             setValidPassword(ValidationStates.VALID)
         }
-        else if(password.length > 0) {
+        else if (password.length > 0) {
             setValidPassword(ValidationStates.INVALID)
         }
     }, [password])
 
     return (
-        <View style={ [styles.container, {backgroundColor:theme.background}] }>
-            <View style={ [styles.form, {backgroundColor:theme.background}] }>
-            <ThemedText style={ styles.heading }>
-                Sign up for an account
-            </ThemedText>
-            <ThemedText>Email</ThemedText>
-            <TextInput 
-                style={ 
-                    (validEmail === ValidationStates.NONE ) ? styles.input : 
-                    (validEmail == ValidationStates.VALID ) ? styles.validInput : styles.invalidInput } 
-                value={email}
-                onChangeText={ (value:string) => setEmail(value) }
-                placeholder="user@example.com"
-            />
-             <ThemedText>Password</ThemedText>
-            <TextInput 
-                style={ 
-                    (validPassword === ValidationStates.NONE ) ? styles.input : 
-                    (validPassword == ValidationStates.VALID ) ? styles.validInput : styles.invalidInput } 
-                secureTextEntry={true}
-                onChangeText={ (value:string) => setPassword(value) }
-                placeholder="Minimum 8 characters"
-            />
-            <Pressable 
-                style={ (validEmail == ValidationStates.VALID 
-                    && validPassword == ValidationStates.VALID) ? styles.button : styles.buttonDisabled }
-                disabled={ 
-                (validEmail == ValidationStates.VALID 
-                    && validPassword == ValidationStates.VALID) ? false : true
-                }
-            >
-                <Text 
-                    style={ (validEmail == ValidationStates.VALID 
-                    && validPassword == ValidationStates.VALID) ? 
-                    styles.buttonText : styles.buttonTextDisabled }>
+        <ThemedView style={styles.container}>
+            <View style={[styles.form, { backgroundColor: theme.background }]}>
+                <ThemedText style={styles.heading}>
+                    Sign up for an account
+                </ThemedText>
+                <ThemedText>Email</ThemedText>
+                <ThemedInput
+                    style={
+                        (validEmail === ValidationStates.NONE) ? styles.input :
+                            (validEmail == ValidationStates.VALID) ? styles.validInput : styles.invalidInput}
+                    value={email}
+                    onChangeText={(value: string) => setEmail(value)}
+                    placeholder="user@example.com"
+                />
+                <ThemedText>Password</ThemedText>
+                <ThemedInput 
+                    style={
+                        (validPassword === ValidationStates.NONE) ? styles.input :
+                            (validPassword == ValidationStates.VALID) ? styles.validInput : styles.invalidInput}
+                    secureTextEntry={true}
+                    onChangeText={(value: string) => setPassword(value)}
+                    placeholder="Minimum 8 characters"
+                />
+                <Pressable
+                    style={(validEmail == ValidationStates.VALID
+                        && validPassword == ValidationStates.VALID) ? styles.button : styles.buttonDisabled}
+                    disabled={
+                        (validEmail == ValidationStates.VALID
+                            && validPassword == ValidationStates.VALID) ? false : true
+                    }
+                >
+                    <Text
+                        style={(validEmail == ValidationStates.VALID
+                            && validPassword == ValidationStates.VALID) ?
+                            styles.buttonText : styles.buttonTextDisabled}>
                         Signup
-                </Text>
-            </Pressable>
-            <Link href="/login">
-                <ThemedText>Have an account? Go to Login</ThemedText>
-            </Link>
+                    </Text>
+                </Pressable>
+                <Link href="/login">
+                    <ThemedText>Have an account? Go to Login</ThemedText>
+                </Link>
+
             </View>
-        </View>
+        </ThemedView>
+
     )
 }
 
@@ -83,7 +87,7 @@ const styles = StyleSheet.create({
         flex: 1
     },
     form: {
-        marginHorizontal:10,
+        marginHorizontal: 10,
         marginVertical: 50,
         backgroundColor: "#FFFFFF",
         padding: 15,
---
+++ components/ThemedInput.tsx
@@ -0,0 +1,16 @@
+import { TextInput, TextInputProps } from "react-native";
+import { useThemeColors } from "@/hooks/useThemeColors";
+
+export function ThemedInput( props:TextInputProps ) {
+    const theme = useThemeColors()
+    return(
+        <TextInput 
+        {...props} 
+        style={[ props.style, { 
+            backgroundColor: theme.background, 
+            borderColor: theme.border, 
+            color: theme.text }
+         ]}
+        />
+    )
+}

ce4ea24fe399ccfb53fafb59c3dd08193f41c1ff
johannesmx <johannes.muljana@ait.nsw.edu.au>
2026-03-19 22:35:36 +0000

    update to login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,11 +7,20 @@ export default function RootLayout() {
   <Stack>
     <Stack.Screen 
     name="index" 
-    options={{title: "Sign up", headerShown: true, 
-    headerStyle: {backgroundColor: theme.background},
-    headerTintColor: theme.text }}/>
-    <Stack.Screen name="test" options={{title: "Profile", headerShown: true}}/>
-    <Stack.Screen name="signup" options={{title: "Home"}}/>
+    options={{
+      title: "Sign up", 
+      headerShown: true, 
+      headerStyle: {backgroundColor: theme.background},
+      headerTintColor: theme.text }}
+    />
+    <Stack.Screen 
+    name="login" 
+    options={{
+      title: "Sign in", 
+      headerShown: true, 
+      headerStyle: {backgroundColor: theme.background},
+      headerTintColor: theme.text }}
+    />
   </Stack>
   )
 }

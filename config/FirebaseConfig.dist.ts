57bc049081452537b7a98a2d7ed23f0b57b735fb
johannes <jmuljana@gmail.com>
2026-03-27 00:08:33 +1100

    update

---
+++ config/FirebaseConfig.dist.ts
@@ -0,0 +1,9 @@
+// rename to FirebaseConfig.ts and put values for keys
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};

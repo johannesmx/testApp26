30bb48fa20b556ac00eeb070980ce97f0db0b62f
johannesmx <johannes.muljana@ait.nsw.edu.au>
2026-03-26 08:11:15 +0000

    latest changes

---
+++ app/main/signout.tsx
@@ -0,0 +1,31 @@
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedButton } from "@/components/ThemedButton";
+import { StyleSheet } from "react-native";
+import { useFirebaseAuth } from "@/hooks/useFirebaseAuth";
+import { router } from "expo-router";
+import { useEffect } from "react";
+
+
+export default function HomeScreen() {
+    const auth = useFirebaseAuth()
+
+    useEffect( () => {
+        if( !auth.isAuthenticated ) {
+            console.log( auth.isAuthenticated)
+            router.replace("/login")
+        }
+    }, [auth.isAuthenticated])
+
+    return (
+        <ThemedView style={{ flex: 1 }}>
+            <ThemedText>Sign out</ThemedText>
+            <ThemedButton
+                text="Sign out"
+                valid={true}
+                handler={() => { auth.signOff() }}
+                disabled={ false }
+            />
+        </ThemedView>
+    )
+}

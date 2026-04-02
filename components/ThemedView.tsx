deff8c05299688ab1e337ca80b3f877de188100b
johannesmx <johannes.muljana@ait.nsw.edu.au>
2026-04-02 04:26:38 +0000

    code cleanup

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -3,7 +3,6 @@ import { useThemeColors } from '@/hooks/useThemeColors'
 
 export function ThemedView( props:ViewProps) {
     const theme = useThemeColors()
-    
     return(
         <View style={[ props.style, { backgroundColor: theme.background }]}>
             { props.children }

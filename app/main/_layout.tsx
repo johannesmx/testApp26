b76a2b6b106b21de5512eb3db81687161ebe9537
johannesmx <johannes.muljana@ait.nsw.edu.au>
2026-04-02 04:26:06 +0000

    fixes for Drawer styles

--- app/main/_layout.tsx
+++ app/main/_layout.tsx
@@ -5,13 +5,24 @@ export default function MainLayout() {
     const theme = useThemeColors()
 
     return (
-        <Drawer>
+        <Drawer screenOptions={{
+            drawerStyle: {backgroundColor: theme.background }, 
+            drawerLabelStyle: {color: theme.text },
+            headerStyle: {backgroundColor: theme.background},
+            headerTintColor: theme.text
+        }}>
             <Drawer.Screen name="home" options={{
                 drawerLabel: "Home",
                 title: "Home",
-                drawerStyle: {backgroundColor: theme.background},
-                headerStyle: {backgroundColor: theme.background},
+                // drawerStyle: {backgroundColor: theme.background},
+                // headerStyle: {backgroundColor: theme.background},
                 headerTintColor: theme.text 
+            }}/>
+             <Drawer.Screen name="signout" options={{
+                drawerLabel: "Sign out",
+                title: "Sign out",
+                // drawerStyle: {backgroundColor: theme.background},
+                
             }}/>
         </Drawer>
     )

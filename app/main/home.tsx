3eeba7b62a753d5b9a46d6cd5ed47a82f5c8c98c
johannesmx <johannes.muljana@ait.nsw.edu.au>
2026-04-02 04:26:50 +0000

    adding screen details and modal

--- app/main/home.tsx
+++ app/main/home.tsx
@@ -1,30 +1,67 @@
-import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Modal, Pressable } from "react-native";
 import { useAuth } from "@/contexts/firebaseAuthContext";
 import { useEffect, useState } from "react";
+import { router } from "expo-router";
+import { ThemedButton } from "@/components/ThemedButton";
+import { useThemeColors } from "@/hooks/useThemeColors";
+
+
 
 export default function HomeScreen() {
     const [authState,setAuthState] = useState<boolean>(true)
+    const [gameVisible, setGameVisible] = useState<boolean>(false)
+    const theme = useThemeColors()
     const auth = useAuth()
 
     useEffect(() => {
         if (!auth.isAuthenticated) {
-            console.log( auth.user)
             setAuthState(false)
         }
     }, [auth.isAuthenticated])
 
     useEffect(() => {
         if(!authState) {
-           // router.replace("/login")
+           router.replace("/login")
         }
     }, [authState])
 
     return (
-        <ThemedView style={{ flex: 1 }}>
-            <ThemedText>Home</ThemedText>
+        <ThemedView style={[styles.container,{ flex: 1 }]}>
+            <ThemedText>Welcome</ThemedText>
+            <Pressable 
+                style={ [styles.startbutton, {backgroundColor: theme.valid }]}
+                onPress={ ()=> { setGameVisible(true) }}
+            >
+                <ThemedText style={ styles.startbuttonText }>Start a new game</ThemedText>
+            </Pressable>
+            <Modal
+                visible={gameVisible}
+                transparent={false}
+                animationType="slide"
+            >
+                <ThemedView style={{flex: 1, padding: 15}}>
+                    <ThemedText>Modal Content</ThemedText>
+                    <Pressable onPress={()=> {setGameVisible(false)}}>
+                        <ThemedText>Close Modal</ThemedText>
+                    </Pressable>
+                </ThemedView>
+            </Modal>
         </ThemedView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+    },
+    startbutton: {
+        padding: 5,
+        marginVertical: 10,
+    },
+    startbuttonText: {
+        textAlign: "center"
+    },
+})

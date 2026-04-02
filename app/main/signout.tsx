5ad337e6f2d8be92410b4a2d5c181b921f70f26d
johannesmx <johannes.muljana@ait.nsw.edu.au>
2026-04-02 04:26:22 +0000

    style fixes

--- app/main/signout.tsx
+++ app/main/signout.tsx
@@ -1,8 +1,11 @@
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
 import { ThemedButton } from "@/components/ThemedButton";
 import { StyleSheet } from "react-native";
 import { useAuth } from "@/contexts/firebaseAuthContext";
-import { router } from "expo-router";
+import { router, Link } from "expo-router";
 import { useEffect } from "react";
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 
 export default function HomeScreen() {
@@ -16,14 +19,35 @@ export default function HomeScreen() {
     }, [auth.isAuthenticated])
 
     return (
-        <ThemedView style={{ flex: 1 }}>
-            <ThemedText>Sign out</ThemedText>
+        <ThemedView style={[ styles.container, { flex: 1}]}>
+            <ThemedText style={ styles.heading }>Sign out of your account</ThemedText>
+            <ThemedText>You will be redirected to the login screen</ThemedText>
             <ThemedButton
-                text="Sign out"
+                text="Press to sign out"
                 valid={true}
                 handler={() => { auth.signOff() }}
                 disabled={ false }
             />
+            <ThemedText style={styles.metatext}>Got here accidentally?</ThemedText>
+            <Link href="/">
+                <ThemedText style={styles.link}><Ionicons name="home" size={16} /> Go back to Home screen</ThemedText>
+            </Link>
         </ThemedView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+       paddingHorizontal: 15,
+       paddingVertical: 10,
+    },
+    heading: {
+        fontSize: 18,
+    },
+    metatext: {
+        marginVertical: 10,
+    },
+    link: {
+        fontWeight: 700,
+    },
+})
